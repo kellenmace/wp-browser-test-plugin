2b58e48cae21387df7fe956cc42e8cb1f3f1e4ce
Kellen Mace <kellen.mace@gmail.com>
2019-05-15 10:23:40 -0400

    Create test plugin, init Composer and require wp-browser as a dev dependency

---
+++ my-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: My Cool Plugin
+ * Version:     0.1.0
+ * Author:      Harness Software
+ * Author URI:  https://harnessup.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+function my_cool_plugin_function() {
+    return 'hello';
+}
